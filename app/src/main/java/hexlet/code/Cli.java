862e56fbed90b1f63fbf7fb52d208dd91c0b659c
Александр <lapshinlsk@gmail.com>
2023-11-26 14:44:32 +0300

    add greeting

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,12 @@
+package hexlet.code;
+import java.util.Scanner;
+
+public class Cli {
+    public static void greeting() {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("May I have your name?");
+        String userName = s.next();
+        System.out.println("Hello, " + userName);
+    }
+}
